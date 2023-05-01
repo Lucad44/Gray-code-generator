63d01fe64e7766c70c1da1018894bbc3e459ec1a
ReadyIce-jpg <83450836+ReadyIce-jpg@users.noreply.github.com>
2023-05-01 12:33:32 +0200

    Update gray.py

--- gray.py
+++ gray.py
@@ -1,4 +1,3 @@
-from tabulate import tabulate
 def grayCode(n: int):
     if n == 1:
         return [0, 1], [0, 1]
@@ -15,24 +14,3 @@ def bin2gray(n: int):
     gray = [binary.pop(0)]
     gray += [str(int(b) ^ int(x)) for b, x in zip(binary, xor)]
     return ''.join(gray)
-
-def main():
-    print("This program can generate the Gray encoding with n bits,\nand convert a single decimal number to Gray code as well.\n")
-    while True:
-        n = input("Enter the number of bits: ")
-        if n.isdigit() and int(n) >= 1:
-            break
-        print("Enter a valid number")
-    bina, dec = grayCode(int(n))
-    table = zip(bina, dec)
-    print(tabulate(table, headers=["Binary", "Decimal"], tablefmt='psql'))
-    while True:
-        n = input("Enter a base 10 number: ")
-        if n.isdigit():
-            break
-        print("Enter a valid number")
-    print(f"\nGray code of {n}d ({''.join(list(bin(int(n))))[2:]}b): {bin2gray(int(n))}")
-
-
-if __name__ == "__main__":
-    main()
